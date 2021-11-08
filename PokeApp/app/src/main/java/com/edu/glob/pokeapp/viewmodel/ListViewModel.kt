35484c3a34bc29dc621cee645b99368c9331613f
Yair Valbuena <yair.valbuena@globant.com>
2021-11-08 16:41:03 -0500

    implement viewmodel with some mockdata before retrofit

--- PokeApp/app/src/main/java/com/edu/glob/pokeapp/viewmodel/ListViewModel.kt
+++ PokeApp/app/src/main/java/com/edu/glob/pokeapp/viewmodel/ListViewModel.kt
@@ -1,6 +1,33 @@
 package com.edu.glob.pokeapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.edu.glob.pokeapp.Pokemon
 
 class ListViewModel: ViewModel() {
+
+    //livedata
+    val pokemons = MutableLiveData<List<Pokemon>>()
+    val pokemonLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh(){
+        fetchPokemons()
+    }
+    //evade of showing the actual method
+    private fun fetchPokemons(){
+
+        //add mockdata for testing before retrofit
+        val mockData = listOf(Pokemon("pokemon 1"),
+            Pokemon("pokemon 2"),
+            Pokemon("pokemon 3"),
+            Pokemon("pokemon 4"),
+            Pokemon("pokemon 5"),
+            Pokemon("pokemon 6"),
+            Pokemon("pokemon 7"),)
+
+        pokemonLoadError.value= false
+        loading.value= false
+        pokemons.value = mockData
+    }
 }
